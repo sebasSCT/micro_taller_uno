00ae7aead077312194190f013c2ba52330b6325d
SebasSCT <sebastiancarmonat.sct@gmail.com>
2024-08-08 09:35:30 -0500

    version 0

---
+++ src/main/java/com/microservicios/talleruno/TallerunoApplication.java
@@ -0,0 +1,13 @@
+package com.microservicios.talleruno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerunoApplication.class, args);
+	}
+
+}
